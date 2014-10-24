f8fa7e739d95785bf82206ee692ab10fd3b6a536
Craig Loomis <cloomis@astro.princeton.edu>
2014-10-24 15:50:22 -0400

    Limit to eupspkg calls to avoid interpolation errors.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""Install this package. Requires sdss3tools.
+
+To use:
+python setup.py install
+"""
+import sdss3tools
+
+sdss3tools.setup(
+    description = "Common code base for PFS MHS actor system",
+    name = "ics_mhs_config"
+)
