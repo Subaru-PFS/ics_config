ec6344892d1223143052d6fee592a177f9798ffb
Craig Loomis <cloomis@astro.princeton.edu>
2017-06-29 02:13:50 -0400

    Added namespace and always interpolate all strings

--- python/config/yamlConfig.py
+++ python/config/yamlConfig.py
@@ -8,22 +8,28 @@ import ruamel_yaml
 
 
 class YamlConfig(object):
-    def __init__(self, actor, root=None, logger=None, logLevel=logging.INFO):
+    def __init__(self, topLevel, root=None,
+                 namespace=None,
+                 logger=None, logLevel=logging.INFO):
         """
         Container for a tree of configuration files.
 
         Args
         ----
-        actor : string (or None)
-           The controlling actor's name (e.g. enu, ccd, mcs)
+        topLevel : string (or None)
+           The controlling topLevel's name (e.g. pfs, enu, ccd, mcs)
         root : string
-           The configuration tree's root directory. By default, the ics_config products's config/ directory
+           The configuration tree's root directory. By default, the ics_config 
+           products's config/ directory
+        namespace : dict
+           A dictionary through which yaml string variables
+           are evaluated.
         logger : logging.Logger
            By default, the logger named 'config'
         logLevel : int
 
         By default, configuration entries are indexed by a
-        dot-delimited name starting with the controlling actor's name
+        dot-delimited name starting with the controlling topLevel's name
 
         Internally, we keep a dictionary of nodes keyed by the partial
         dot-delimited names, where the values are (fullpath, content)
@@ -46,23 +52,56 @@ class YamlConfig(object):
                 root = os.path.join(configRoot, 'config')
 
         self.root = root
-        self.actor = actor
+        self.topLevel = topLevel
+
+        self.namespace = namespace
 
         self.flush()
 
     @property
     def _rootPath(self):
-        if self.actor is None:
+        if self.topLevel is None:
             return self.root
         else:
-            return os.path.join(self.root, self.actor)
+            return os.path.join(self.root, self.topLevel)
 
     def flush(self):
         """ Cause all our configuration to be reloaded from disk files. """
 
         self.configs = dict()
 
-    def _loadOne(self, parts):
+    def _yamlPath(self, parts):
+        return os.path.join(self._rootPath, *parts)
+
+    def finalNamespace(self, extraNamespace=None):
+        if extraNamespace is not None:
+            namespace = self.namespace.copy() if self.namespace is not None else dict()
+            namespace.update(extraNamespace)
+        else:
+            namespace = self.namespace
+
+        return namespace
+    
+    def loadYamlFile(self, yamlPath, extraNamespace=None):
+        """ Return a YAML loader which interpolates strings with our namespace. """
+
+        if not os.path.isfile(yamlPath):
+            raise ValueError('not a file path: %s' % (yamlPath))
+
+        namespace = self.finalNamespace(extraNamespace)
+
+        with open(yamlPath, 'r') as yamlFile:
+            loader = ruamel_yaml.Loader(yamlFile)
+            if namespace is not None:
+                def interpolateString(loader, node, namespace=namespace):
+                    return string.Template(node.value).substitute(namespace)
+
+                loader.add_constructor(u'tag:yaml.org,2002:str', interpolateString)
+
+            cfg = loader.get_data()
+            return cfg
+
+    def _loadOne(self, parts, extraNamespace=None):
         """ returns the value for a perhaps partial list of name parts. 
 
         Args
@@ -73,44 +112,48 @@ class YamlConfig(object):
         Returns:
            content : None if a directory, parsed yaml content otherwise.
         """
-        
+
         name = '.'.join(parts)
         try:
             return self.configs[name][1]
         except KeyError:
-            path = os.path.join(self._rootPath, *parts)
+            path = self._yamlPath(parts)
             if os.path.isdir(path):
                 self.configs[name] = (path, None)
                 return None
+
             yamlPath = "%s.yaml" % (path)
-            if os.path.isfile(yamlPath):
-                with open(yamlPath, 'r') as yamlFile:
-                    cfg = ruamel_yaml.load(yamlFile)
-                    self.configs[name] = (yamlPath, cfg)
-                    return cfg
+            cfg = self.loadYamlFile(yamlPath, extraNamespace=extraNamespace)
+            self.configs[name] = (yamlPath, cfg)
 
-            raise KeyError('do not know what %s is' % (name))
+            return cfg
 
-    def _loadOnDemand(self, path):
+    def _loadOnDemand(self, path, extraNamespace=None):
         parts = path.split('.')
         for i in range(len(parts)):
-            cfg = self._loadOne(parts[0:i+1])
+            cfg = self._loadOne(parts[0:i+1], extraNamespace=extraNamespace)
             self.logger.debug('loaded %s: %s' % (parts[0:i+1], cfg))
             if cfg is not None:
                 return cfg, parts[i+1:]
 
         raise KeyError('%s is not contained in a configuration file' % path)
 
-    def get(self, path, interpEnv=None):
+    def rawConfig(self, path):
+        """ Return the raw YAML file """
+
+        with open(self._yamlPath(path.split('.'))+'.yaml', 'r') as f:
+            return '\n'.join(f.readlines())
+
+    def get(self, path, extraNamespace=None, flush=False):
         """ Return the configuration value for the given dotted path. 
 
         Args
         ----
         path : dot-delimited string
           The name of the configuration variable to load.
-        interpEnv : dict-like
-          If passed in, string values are treated as string.Templates,
-          evaluated using this dictionary.
+        extraNamespace : dict-like
+           update self.namespace with this before interpolating.
+           If s
 
         Returns
         -------
@@ -121,14 +164,16 @@ class YamlConfig(object):
         KeyError 
         """
 
-        if interpEnv is not None:
-            path = string.Template(path).substitute(interpEnv)
+        namespace = self.finalNamespace(extraNamespace)
+        cfg, cfgParts = self._loadOnDemand(path, extraNamespace=extraNamespace)
+        for n_i, name in enumerate(cfgParts):
+            finalName = string.Template(name).substitute(namespace)
+            try:
+                cfg = cfg[finalName]
+            except KeyError:
+                raise KeyError('%s not found in %s' % (finalName, path))
+            
+        if extraNamespace is not None:
+            self.flush()
 
-        cfg, cfgParts = self._loadOnDemand(path)
-        for name in cfgParts:
-            cfg = cfg[name]
-
-        if not isinstance(cfg, (str, unicode)) or interpEnv is None:
-            return cfg
-
-        return string.Template(cfg).substitute(interpEnv)
+        return cfg
